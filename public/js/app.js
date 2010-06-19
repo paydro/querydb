4dcefcd32379f2680b198a310e7e26331b5e7bd6
Peter Bui <peter@paydrotalks.com>
2010-06-18 19:51:48 -0500

    Add 1-level namescopes for keys combos

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,11 @@
 var keyLogger = function(e){
-    console.log("KeyPress");
+    // console.log("KeyPress");
     console.log("keyCode: " + e.keyCode);
-    console.log("meta: " + e.metaKey);
-    console.log("alt: " + e.altKey);
-    console.log("ctrl: " + e.ctrlKey);
-    console.log(e.target);
+    // console.log("meta: " + e.metaKey);
+    // console.log("alt: " + e.altKey);
+    // console.log("ctrl: " + e.ctrlKey);
+    // console.log("shift: " + e.shiftKey);
+    // console.log(e);
 };
 
 var QueryStore = {
@@ -26,6 +27,9 @@ var QueryStore = {
 
         return hashes;
     },
+    clear: function(){
+        localStorage.clear();
+    },
 
 };
 
@@ -35,7 +39,6 @@ QueryBuilder = {
         return $("thead tr th:nth-child(" + headerIndex + ")").text();
     },
     tableize: function(str){
-        console.log(str);
         return str.replace(/_id/, "s");
     },
     findParentRecord: function(element){
@@ -49,43 +52,169 @@ QueryBuilder = {
     },
 }
 
-var bindKeys = function(keyDefinition){
-    var r = $("#results");
+var KeyManager = function(){
+    var mappings = {};
+    var state = "";
 
-    for(key in keyDefinition){
-        console.log("Binding " + key + " to " + keyDefinition[key]);
-        r.bind(key, keyDefinition[key]);
+    var inState = function(){
+        return state !== "";
+    };
+
+    var addState = function(s){
+        state += s;
+    };
+    var setState = function(s){
+        state = s;
+    };
+
+    // DEBUG
+    this.defs = function(){
+        return mappings;
+    };
+    this.showDefs = function(){
+        console.log(mappings);
+    };
+
+    this.showState = function(){
+        console.log(state);
+    };
+    // END DEBUG
+
+    // Find function to call for a given key
+    this.find = function(eventKey){
+        if(inState()){
+            return mappings["state-" + state][eventKey];
+        }
+        else {
+            return mappings[eventKey];
+        }
+    };
+
+    // <3 closures
+    var stateSetFn = function(k){
+        return (function(){
+            addState(k);
+        });
+    };
+
+    // Define key actions
+    // TODO: Make it recursive to define multiple layers of namespaces
+    this.define = function(definition){
+        for(key in definition){
+            if(typeof(definition[key]) === "object"){
+                mappings[key] = stateSetFn(key);
+                mappings["state-" + key] = definition[key];
+            }
+            else {
+                mappings[key] = definition[key];
+            }
+        }
+    };
+
+    this.resetState = function(){
+        setState("");
     }
-};
 
+};
+var keyManager = new KeyManager();
+keyManager.define({
+    // Focus on query textarea
+    "/": function(event){ $("#query form textarea").focus(); event.preventDefault(); },
+
+    // History navigation
+    "n": function(){ window.location.hash = hashes.next() },
+    "p": function(){ window.location.hash = hashes.prev() },
+
+    // Table navigation
+    // NOTE: Need so that I don't have to create a function here, but just give it
+    // the function name - i.e., Navigator.moveLeft. Navigator is using "this"
+    // and when not inside a function like below, this means the element. FAIL.
+    "h": function(){ Navigator.moveLeft(); },
+    "l": function(){ Navigator.moveRight(); },
+    "j": function(){ Navigator.moveDown(); },
+    "k": function(){ Navigator.moveUp(); },
+    "g": {
+        // Not working yet
+        // "g": {
+            // "t": function() { console.log("double ggt"); },
+        // },
+        "t": function(){ console.log('go to table'); },
+    },
+
+    // Go to parent table for foreign key
+    "<CR>": function(){ QueryBuilder.findParentRecord($("#results .selected")); },
+});
+
+var KeyTranslator = {
+    isModifierPressed: function(e){
+        // 13 = enter
+        return (e.metaKey || e.ctrlKey || e.altKey || (e.keyCode === 13));
+    },
+    codeToKey: function(code, shift){
+        if(shift){
+            return KeyTranslator.upperCase[code];
+        }
+        else {
+            return KeyTranslator.lowerCase[code];
+        }
+    },
+
+    // Return an arrawy of modifier keys
+    modifierKeys: function(e){
+        var modifiers = [];
+        // Ordering of modifiers is important
+        if(e.altKey){
+            modifiers.push("M");
+        }
+        if(e.ctrlKey){
+            modifiers.push("C");
+        }
+        if(e.metaKey){
+            modifiers.push("D");
+        }
+        return modifiers;
+    },
+    translate: function(e){
+        var key;
+
+        key = KeyTranslator.codeToKey(e.keyCode, e.shiftKey);
+        if(KeyTranslator.isModifierPressed(e)){
+           var modifiers = KeyTranslator.modifierKeys(e);
+           modifiers.push(key);
+           key = "<" + modifiers.join("-") + ">";
+        }
+
+        return key;
+    },
+    upperCase: {
+        13: "CR",
+        191: "?",
+        78: "N",
+        80: "P",
+        72: "H",
+        76: "L",
+        74: "J",
+        75: "K",
+    },
+    lowerCase: {
+        13: "CR",
+        191: "/",
+        78: "n",
+        80: "p",
+        72: "h",
+        76: "l",
+        74: "j",
+        75: "k",
+        71: "g",
+        84: "t",
+    },
+};
 
 // ***************
 // Onload function
 // ***************
 $(function(){
     hashes = new HashStack();
-
-    bindKeys({
-        // Focus on query textarea
-        "/": function(){
-            $("#query form textarea").focus();
-            // Prevent "/" char appearing in textarea
-            e.preventDefault();
-        },
-
-        // History navigation
-        "n": function(){ window.location.hash = hashes.next() },
-        "p": function(){ window.location.hash = hashes.prev() },
-
-        // Table navigation
-        "h": function(){ Navigator.moveLeft(); },
-        "l": function(){ Navigator.moveRight(); },
-        "j": function(){ Navigator.moveDown(); },
-        "k": function(){ Navigator.moveUp(); },
-
-        // Go to parent table for foreign key
-        "<CR>": function(){ QueryBuilder.findParentRecord($("#results .selected")); },
-    });
 
     // Make the body the focus on load to allow for keyboard interaction
     $("#results").focus();
@@ -111,69 +240,27 @@ $(function(){
         }
     });
 
+    // $("body").live("keydown", function(e){
+        // keyLogger(e);
+    // });
     // Focus on query box with "/"
-    $("body").live("keypress", function(e){
-        keyLogger(e);
+    $("body").live("keydown", function(e){
         var r = $("#results");
+        keyLogger(e);
 
+        if(e.keyCode == 27){
+            keyManager.resetState();
+        }
         if(e.target.nodeName.toLowerCase() == "textarea"){
             return; // Do nothing when inside the textarea
         }
 
-        var keyTranslation = {
-            13: "<CR>",
-            47: "/",
-            110: "n",
-            112: "p",
-            104: "h",
-            108: "l",
-            106: "j",
-            107: "k",
-        };
-        switch(e.keyCode){
-            // case 47: // "/"
-                // $("#query form textarea").focus();
-                // // Prevent "/" char appearing in textarea
-                // e.preventDefault();
-                // break;
-
-            // // Parent lookup for foreign key
-            // case 13:
-                // QueryBuilder.findParentRecord($("#results .selected"));
-                // break;
-
-            default:
-                console.log("Triggering: " + keyTranslation[e.keyCode]);
-                r.trigger(keyTranslation[e.keyCode]);
-                break;
-
-            // NAVIGATION KEYS
-            // case 110: // "n"
-                // r.trigger("n");
-                // // window.location.hash = hashes.next();
-                // break;
-            // case 112: // "p"
-                // r.trigger("p");
-                // // window.location.hash = hashes.prev();
-                // break;
-            // case 104: // "h"
-                // r.trigger("h");
-                // // Navigator.moveLeft();
-                // break;
-            // case 108: // "l"
-                // r.trigger("l");
-                // // Navigator.moveRight();
-                // break;
-            // case 106: // "j"
-                // r.trigger("j");
-                // // Navigator.moveDown();
-                // break;
-            // case 107: // "k"
-                // r.trigger("k");
-                // // Navigator.moveUp();
-                // break;
-
-        } // End switch statement
+        // TODO: Remove this conditional when all keys are mapped
+        var eventKey = KeyTranslator.translate(e);
+        if(typeof eventKey !== "undefined"){
+            var fn = keyManager.find(eventKey);
+            if(typeof fn !== "undefined"){ fn(e); }
+        }
     });
 
     $("#results td").live("click", function(){
